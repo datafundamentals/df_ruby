ff9d4fb9eab7d2590e8b61b48d6b88cd99983d34
jcarapet <jeffcarapetyan@gmail.com>
2014-06-11 12:05:58 -0500

    adjusted serverspec tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,4 +3,4 @@ include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
 describe command('ruby -v') do 
-	it { should return_stdout 'ruby 1.9.3p484 (2013-11-22 revision 43786) [x86_64-linux]'}
+	it { should return_stdout 'ruby 1.9.3p484'}
